bab9835fa21b8cb48a6963042f7cf1670838efdc
rishikadevanand <rishidev2107@gmail.com>
2025-11-27 18:10:29 +0530

    Deleted wrong placeholder Problem_04, Problem_05, Problem_06

--- Day04/Problem_04.java
+++
@@ -1,5 +0,0 @@
-﻿public class Problem_04 {
-    public static void main(String[] args) {
-        System.out.println("Problem_04 placeholder - interface + menu example.");
-    }
-}
--- Day04/Problem_05.java
+++
@@ -1,5 +0,0 @@
-﻿public class Problem_05 {
-    public static void main(String[] args) {
-        System.out.println("Problem_05 placeholder - add your code here.");
-    }
-}
