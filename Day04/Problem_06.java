689f8cabaa8fa9a003e450b748b7ff62276341bd
rishikadevanand <rishidev2107@gmail.com>
2025-11-27 18:07:11 +0530

    Added correct Problem_04, Problem_05, Problem_06 code

--- Day04/Problem_06.java
+++ Day04/Problem_06.java
@@ -1,5 +1,33 @@
-﻿public class Problem_06 {
+class Vehicle {
+    void start() {
+        System.out.println("Vehicle is starting...");
+    }
+}
+
+class Car extends Vehicle {
+    void start() {
+        System.out.println("Car is starting...");
+    }
+
+    void carOnlyFeature() {
+        System.out.println("Car has AC and Music System");
+    }
+}
+
+public class Problem_06
+ {
     public static void main(String[] args) {
-        System.out.println("Problem_06 placeholder - add your code here.");
+
+        // Upcasting (Car → Vehicle reference)
+
+        Vehicle v = new Car();
+        v.start();             // Calls Car’s start() because object is Car
+
+        //Downcasting (Vehicle → Car)
+
+        Car c = (Car) v;
+
+        c.start();           // Calls Car’s start()
+        c.carOnlyFeature();  // Now we can access Car specific method
     }
 }
